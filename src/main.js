d2ace2fbae5f0e328ef8621a326e8e915f38adbc
Nick Meyer <nick@e3b0c442.io>
2020-11-01 23:17:26 -0600

    Add Now UI and clean up source formatting

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from "vue";
 import App from "./App.vue";
+import NowUiKit from "./plugins/now-ui-kit";
 
 Vue.config.productionTip = false;
 
+Vue.use(NowUiKit);
 new Vue({
   render: h => h(App)
 }).$mount("#app");
